f37be32da3102ad7a8bda0fa96d94fac25d8b49f
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2012-12-20 16:34:44 -0500

    adding source files

--- src/WordGame.java
+++ src/WordGame.java
@@ -1,14 +1,23 @@
+import java.util.*;
+import javax.swing.JFrame;
+
+
 
 public class WordGame {
 	
-	
-
 	public static void main(String[] args) {
 		//load library
 		Library mlib = new Library();
 		int size = mlib.getmWords().length;
-		
+		Random rand = new Random(size);
+		String[] words= mlib.getmWords();
 		System.out.println("There are "+ size+" words available");
+		
+		
+		//pick the work
+		String answer = words[rand.nextInt(size+1) ];
+		System.out.println("The word is " + answer);
+		JFrame main = new JFrame();
 		
 	}
 
