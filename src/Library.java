848d7048640a181eed310bf877f5190a3f71b4ed
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2012-12-20 16:08:03 -0500

    initial commit of game source

---
+++ src/Library.java
@@ -0,0 +1,73 @@
+public class Library {
+
+	
+public Library(){
+		
+	}
+
+
+	public String[] getmWords() {
+	return mWords;
+}
+
+
+
+	public String[] mWords = { "mabby", "macao", "macaw", "macco", "maced", "macer", "maces", "macho",
+			"machs", "macks", "macle", "macro", "madam", "madge", "madia", "madid", "madly",
+			"madre", "mafia", "mages", "magic", "magma", "magot", "magus", "mahdi", "mahoe",
+			"maian", "maids", "mails", "maims", "maine", "mains", "maist", "maize", "major",
+			"maked", "maker", "makes", "malar", "malax", "malay", "maleo", "males", "malet",
+			"malic", "malls", "malma", "malta", "malts", "malty", "malum", "mamas", "mamba",
+			"mambo", "mamie", "mamma", "mammy", "manas", "manca", "maned", "maneh", "manes",
+			"mange", "mango", "mangy", "mania", "manic", "manid", "manie", "manis", "manks",
+			"manly", "manna", "manor", "manos", "manse", "manta", "manto", "manul", "manus",
+			"maori", "maple", "maqui", "marai", "march", "marcs", "mardi", "mares", "marge",
+			"maria", "marie", "marks", "marly", "marry", "marse", "marsh", "marts", "maser",
+			"mashy", "masks", "mason", "massa", "masse", "massy", "masts", "masty", "match",
+			"mated", "mater", "mates", "matey", "maths", "matie", "matin", "matte", "matts",
+			"matzo", "maule", "mauls", "maund", "mauve", "maven", "mavin", "mavis", "mawks",
+			"mawky", "maxim", "maxis", "mayan", "mayas", "maybe", "mayor", "mayst", "mazed",
+			"mazel", "mazer", "mazes", "meach", "meads", "meals", "mealy", "means", "meant",
+			"meany", "mease", "meath", "meats", "meaty", "meawl", "mecca", "mecum", "medal",
+			"media", "medic", "medii", "medle", "medly", "medoc", "meech", "meeds", "meeth",
+			"meets", "meine", "meint", "meiny", "melam", "melba", "melds", "melee", "melic",
+			"melne", "meloe", "melon", "melts", "memos", "mends", "menge", "menow", "mense",
+			"mente", "menus", "meows", "merce", "merci", "mercy", "merer", "meres", "merge",
+			"merit", "merke", "merle", "meros", "merou", "merry", "merus", "mesad", "mesal",
+			"mesas", "mesel", "meshy", "mesne", "meson", "messy", "metal", "meted", "meter",
+			"metes", "metic", "metif", "metis", "metol", "metre", "metro", "mette", "meute",
+			"mewed", "mewls", "mexal", "mezzo", "mhorr", "miami", "miaou", "miaow", "miasm",
+			"miaul", "micas", "miche", "micks", "micro", "midas", "middy", "midge", "midis",
+			"midst", "miens", "miffs", "miffy", "miggs", "might", "mikes", "milan", "milch",
+			"milen", "miler", "miles", "milks", "milky", "mille", "mills", "milos", "mimed",
+			"mimeo", "mimer", "mimes", "mimic", "minae", "minas", "mince", "mincy", "minds",
+			"mined", "miner", "mines", "minge", "mingy", "minim", "minis", "minks", "minny",
+			"minor", "minos", "minow", "mints", "minty", "minum", "minus", "mired", "mires",
+			"mirks", "mirky", "mirth", "mirvs", "mirza", "misdo", "miser", "misle", "misly",
+			"misos", "missa", "missy", "mists", "misty", "miter", "mites", "mitre", "mitts",
+			"mitty", "mixed", "mixen", "mixer", "mixes", "mixup", "mizzy", "moans", "moate",
+			"moats", "mobil", "moble", "mocha", "moche", "mocks", "modal", "model", "modem",
+			"moder", "modes", "modii", "modus", "moeve", "mogul", "mohur", "moile", "moils",
+			"moira", "moire", "moist", "molar", "molds", "moldy", "moled", "moles", "molle",
+			"molls", "molly", "molto", "molts", "momma", "mommy", "momot", "momus", "monad",
+			"monal", "monas", "monde", "mondo", "moner", "money", "monks", "monos", "monte",
+			"month", "mooch", "moods", "moody", "mooed", "moola", "moong", "moons", "moony",
+			"moore", "moors", "moory", "moose", "moote", "moots", "moped", "moper", "mopes",
+			"mopey", "mopsy", "mopus", "moral", "moray", "morel", "mores", "moria", "moric",
+			"moril", "morin", "mormo", "morne", "morns", "moron", "moros", "morph", "morro",
+			"morse", "morus", "morwe", "mosel", "moses", "mosey", "mosks", "mossy", "moste",
+			"mosts", "moted", "motel", "motes", "motet", "motey", "moths", "mothy", "motif",
+			"moton", "motor", "motte", "motto", "motty", "moues", "mould", "moule", "moult",
+			"mound", "mount", "mourn", "mouse", "mousy", "mouth", "moved", "mover", "moves",
+			"movie", "mowed", "mowen", "mower", "moxas", "moxie", "moyle", "mucic", "mucid",
+			"mucin", "mucks", "mucky", "mucor", "mucro", "mucus", "mudar", "muddy", "mudir",
+			"mudra", "muffs", "mufti", "muggs", "muggy", "mugil", "mujik", "mulch", "mulct",
+			"muled", "mules", "muley", "mulla", "mulls", "mulse", "multi", "multo", "mumbo",
+			"mumms", "mummy", "mumps", "munch", "munga", "mungo", "muons", "mural", "mured",
+			"mures", "murex", "murks", "murky", "murre", "murry", "murth", "murza", "musae",
+			"musal", "musar", "musca", "musci", "mused", "muser", "muses", "muset", "mushy",
+			"music", "musit", "musks", "musky", "mussy", "musts", "musty", "mutch", "muted",
+			"muter", "mutes", "mutic", "mutts", "muzzy", "myips", "mylar", "mynah", "mynas",
+			"myoid", "myoma", "myope", "myopy", "myrrh", "mysis", "mythe", "myths" };
+
+}
---
+++ src/WordGame.java
@@ -0,0 +1,15 @@
+
+public class WordGame {
+	
+	
+
+	public static void main(String[] args) {
+		//load library
+		Library mlib = new Library();
+		int size = mlib.getmWords().length;
+		
+		System.out.println("There are "+ size+" words available");
+		
+	}
+
+}
